1801864a895cdd7c9b45fbd4b04bfe2a8ced3740
jprq87 <jprq1987@gmail.com>
2023-02-06 05:28:41 -0500

    Update docker_deploy.py

--- 2_Prefect/flows/03_deployments/docker_deploy.py
+++ 2_Prefect/flows/03_deployments/docker_deploy.py
@@ -6,7 +6,7 @@ docker_block = DockerContainer.load("jprq-zoom")
 
 docker_dep = Deployment.build_from_flow(
     flow=etl_parent_flow,
-    name="docker-flow",
+    name="github-flow",
     infrastructure=docker_block,
 )
 
