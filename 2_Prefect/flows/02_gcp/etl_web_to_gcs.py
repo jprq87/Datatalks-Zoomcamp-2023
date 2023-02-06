b397c5a0fdc0f15fa2fd97d1f9ce9e3b5b3cf112
jprq87 <jprq1987@gmail.com>
2023-02-06 05:52:59 -0500

    Updated files

--- 2_Prefect/flows/02_gcp/etl_web_to_gcs.py
+++ 2_Prefect/flows/02_gcp/etl_web_to_gcs.py
@@ -9,7 +9,6 @@ def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
     return pd.read_csv(dataset_url)
 
-
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
@@ -20,7 +19,6 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
     print(f"rows: {len(df)}")
     return df
 
-
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
@@ -29,14 +27,12 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     df.to_parquet(path, compression="gzip")
     return path
 
-
 @task()
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
     gcs_block = GcsBucket.load("zoom-gcs")
     gcs_block.upload_from_path(from_path=path, to_path=path)
     return
-
 
 @flow()
 def etl_web_to_gcs() -> None:
